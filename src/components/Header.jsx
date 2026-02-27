9c2f7b961560419fc08cafe8fb90e40f44f752a5
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-02-27 15:42:25 +0530

    Implemented Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+         <h1 className="text-3xl font-bold text-center mb-6"> To-Do List Application</h1>
+    </div>
   )
 }
 
