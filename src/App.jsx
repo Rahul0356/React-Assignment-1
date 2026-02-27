b89f656cf591264b49936630b43dbcc3fa86e3a3
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-02-27 15:43:23 +0530

    Added state management in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
+import ToDoList from './components/ToDoList ';
 
 const App = () => {
+  
+   const [todos, setTodos] = useState([]);
+
   return (
-    <div>
-      <Header/>
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <div className="bg-white p-6 rounded shadow w-96">
+        <Header />
+        <ToDoList todos={todos} setTodos={setTodos} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
